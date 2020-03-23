681c35ecd15a1891f85b627da78ac28010eb2ead
Mlodovico <murilo.lodovico@outlook.com>
2020-03-23 00:25:30 -0300

    3.17.25 estilizacao de pagina default

--- src/pages/Order/OrderDashboard/index.js
+++ src/pages/Order/OrderDashboard/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import Icon from 'react-web-vector-icons';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/fastfeet-logo@2x.png'
 
 export default function OrderDashboard() {
-    return <h1>DashBoard</h1>;
+    return (
+      <>
+        <Form class="fasfeet-bar-navigation">
+          <img src={logo} />
+          <strong> | </strong>
+
+          <button class="orderDashboard">Encomendas</button>
+          <button class="deliveruDashboard">Entregadores</button>
+          <button class="recipientDashboard">Destinátario</button>
+          <button class="problemDashboard">Problemas</button>
+        </Form>
+
+        <div>
+          <h3>
+            Gerenciamento de Encomendas
+          </h3>
+
+          <Input
+            name="search"
+            type="search"
+            placeholder="Buscar por Encomendas"
+          />
+
+        </div>
+
+      </>
+    );
 
 }
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,67 @@
 import styled from 'styled-components';
+import { darken} from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #715;
+  background: #D3D3D3;
+  align-items: left;
+  justify-content: left;
+`;
+
+export const Content = styled.div `
+    @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  height: 60px;
+  max-width: 100%;
+  background: #fff;
+  align-items: baseline;
+  border-block-end-color: black;
+
+  img {
+    margin-right: 25px;
+    margin-left: 15px;
+    height: 20px;
+    width: 125px;
+    margin-top: 20px;
+  }
+
+  strong {
+    color: #A9A9A9;
+    font-size: 30px;
+  }
+
+  button {
+    margin-left: 20px;
+    background: #F8F8FF;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    align-items: center;
+    color: #A9A9A9;
+    font-weight: bold;
+    border: 0;
+    padding: 30 40px;
+    transition: color 0.5s;
+
+    &:hover {
+      color: ${darken(0.20, '#A9A9A9')}
+    }
+  }
+
+  div {
+
+    display: row;
+    margin-top: 60px;
+    margin-left: 200px;
+
+    h3 {
+      font-family: 'Roboto', sans-serif;
+      font-size: 20px;
+    }
+
+    input {
+      margin-top: 150px;
+      height: 30px;
+      width: 250px;
+    }
+  }
 `;
